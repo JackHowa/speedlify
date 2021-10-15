92eecd77c3ee50598c48c600787550e2f3b77c47
Jack Howard <jack.howard@washpost.com>
2021-10-15 14:25:12 -0400

    feat: Add jack's personal site, remove extraneous

---
+++ _data/sites/jackhowa.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'jackhowa',
+  description: 'Jackhowa\'s personal website',
+  urls: ['https://jackhowa.com'],
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+    freshChrome: "site"
+	},
+};
