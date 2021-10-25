c77cea4e8296e27423a54af1dde70e3a53ca2b4d
Jack Howard <jack.howard@washpost.com>
2021-10-25 13:01:23 -0500

    feat: Add marvel color switcher

---
+++ _data/sites/marvel-color-switcher.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Marvel Color Switcher',
+  description: 'marvel colors',
+  urls: ['https://marvel-color-picker.netlify.app/'],
+	options: {
+		frequency: 60 * 23 // (in minutes), 23 hours
+	},
+};
